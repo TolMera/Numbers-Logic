07ea00f3377ba99eb3269bd31e167285da2d809c
TolMera <bjorn.macintosh@gmail.com>
2022-08-22 21:47:21 +1000

    Updated the 'isTetrahedral' function so it's a bit better/quicker

--- lib/isTetrahedral.ts
+++ lib/isTetrahedral.ts
@@ -4,17 +4,14 @@ import { positiveOrNegativeSequence } from "./positiveOrNegativeSequence";
 
 export function isTetrahedral(inputN: number | EnhancedNumber): boolean {
 	const n = new EnhancedNumber(inputN);
-	if (n._isTetrahedral) {
-		return n.isTetrahedral as boolean;
+	if (n._isTetrahedral !== undefined) {
+		return n.isTetrahedral;
 	}
 
-	let c: number | boolean = 0;
-	while (true) {
-		let nth = getTetrahedral(c);
-		c = positiveOrNegativeSequence(n.number, nth, c);
-		if (c === true || c === false) {
-			n.isTetrahedral = c;
-			return n.isTetrahedral;
-		}
+	let c: number = 1;
+	let nth = getTetrahedral(c);
+	while (Math.abs(n.number) > nth) {
+		nth = getTetrahedral(++c);
 	}
+	return (n.isTetrahedral = Math.abs(n.number) === nth);
 }
