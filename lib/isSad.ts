28a10bb415f63d56e1f557c9d1e89e6f98345215
TolMera <bjorn.macintosh@gmail.com>
2022-08-25 16:12:09 +1000

    Caching on these number functions

--- lib/isSad.ts
+++ lib/isSad.ts
@@ -4,5 +4,9 @@ import { isHappy } from "./isHappy";
 export function isSad(inputN: number | EnhancedNumber, base: number = 10) {
 	const n = new EnhancedNumber(inputN);
 
-	return !isHappy(n, base);
+	if (n._isSad[base] !== undefined) {
+		return n._isSad[base] as boolean;
+	}
+
+	return n._isSad[base] = !isHappy(n, base);
 }
