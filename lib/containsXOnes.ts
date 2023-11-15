feb07c61e4dedf89f4b23a2f15e226136c5e4bbf
TolMera <bjorn.macintosh@gmail.com>
2023-11-15 19:37:54 +1000

    2023/11/WIP (#13)
    * Add jest test for containsXOnes
    
    * updating with all changes
    
    * latest package.json

--- lib/containsXOnes.ts
+++ lib/containsXOnes.ts
@@ -2,9 +2,6 @@ import { EnhancedNumber } from "../classes/enhancedNumber";
 
 export function containsXOnes(inputN: number | EnhancedNumber, x: number): boolean {
 	const n = new EnhancedNumber(inputN);
-	if (n._containsXOnes !== undefined) {
-		return n.containsXOnes;
-	}
-
-	return n.containsXOnes = n.number.toString(2).replace(/0/g, '').length === x;
+	n._containsXOnes[x] = n._containsXOnes[x] !== undefined ? n._containsXOnes[x] as boolean: n.number.toString(2).replace(/0/g, '').length === x;
+	return n._containsXOnes[x]!;
 }
